22f8d60b182b41ec2da313e4fc5f0974e32fe0ec
angkamilama <angkami46@gmail.com>
2024-09-08 23:44:00 +0200

    add tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -48,66 +48,108 @@ function App() {
   };
 
   return (
-    <div className=" w-screen h-full border border-red-500">
+    <div className=" w-screen h-screen flex flex-col justify-evenly items-center border border-red-300">
       <form
         onSubmit={handleSubmit}
-        className="flex justify-evenly items-center h-[300px] w-[600px] border border-slate-400"
+        className="flex justify-evenly items-center w-[500px] h-[300px] border border-green-300 "
       >
         <label>
           Todo:
           <input
             value={todoItem}
             onChange={(e) => setTodoItem(e.target.value)}
-            className="border-2 border-red-700"
+            className="border border-dotted border-sky-700 rounded-lg ml-2 p-2 outline-none"
           />
         </label>
-        <button type="submit">Add</button>
+        <button
+          type="submit"
+          className="p-2 bg-slate-500 rounded-lg text-slate-100 hover:bg-slate-600"
+        >
+          Add
+        </button>
       </form>
-      <div>
+      <div className="flex flex-col justify-evenly items-center">
         {todoList.map((todo) =>
           todo.todoId === editId ? (
             <div key={todo.todoId}>
-              <form onSubmit={handleEditedSubmit}>
-                <label>
+              <form
+                onSubmit={handleEditedSubmit}
+                className=" flex justify-evenly items-center w-[500px]"
+              >
+                <label className="border border-dashed border-red-300 w-1/2 p-1 ">
                   <input
                     type="text"
                     name="editedTodo"
                     defaultValue={todo.todoItem}
                     onChange={(e) => handleEditedInputTodo(e)}
+                    className="text-center outline-none"
                   />
                 </label>
-                <button type="submit">Update</button>
-                <button onClick={() => setEditId("")}>Cancel</button>
+                <div className="flex justify-evenly items-center w-1/2 ">
+                  <button
+                    type="submit"
+                    className="bg-green-300 rounded-lg px-2 p-1 hover:bg-green-400"
+                  >
+                    Update
+                  </button>
+                  <button
+                    onClick={() => setEditId("")}
+                    className="bg-red-400 rounded-lg px-2 p-1 hover:bg-red-500"
+                  >
+                    Cancel
+                  </button>
+                </div>
               </form>
               {showEditInputItemMsg && (
                 <p className="text-red-600">Please change the TodoItem</p>
               )}
             </div>
           ) : (
-            <>
-              <div key={todo.todoId}>
-                <div>{todo.todoItem}</div>
-                <button
-                  onClick={() => {
-                    setEditId(todo.todoId);
-                  }}
-                >
-                  Edit
-                </button>
-                <button onClick={() => setRemovalId(todo.todoId)}>
-                  Remove
-                </button>
+            <div>
+              <div
+                key={todo.todoId}
+                className=" flex justify-between items-center w-[500px] mb-4"
+              >
+                <div className="border border-dashed border-red-300 w-1/2 p-1 text-center">
+                  {todo.todoItem}
+                </div>
+                <div className="flex justify-evenly items-center w-1/2 ">
+                  <button
+                    onClick={() => {
+                      setEditId(todo.todoId);
+                    }}
+                    className="bg-green-300 rounded-lg px-2 p-1 hover:bg-green-400"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => setRemovalId(todo.todoId)}
+                    className="bg-red-400 rounded-lg px-2 p-1 hover:bg-red-500"
+                  >
+                    Remove
+                  </button>
+                </div>
               </div>
               {removalId === todo.todoId && (
-                <>
-                  <h3>Do you want to remove the todoItem ? </h3>
-                  <button onClick={() => handleRemoveTodo(todo.todoId)}>
+                <div className="w-full flex justify-between items-center mt-3 border border-dashed border-slate-400 p-2 rounded-lg mb-2">
+                  <h3 className="text-slate-800">
+                    Do you want to remove the todoItem ?{" "}
+                  </h3>
+                  <button
+                    onClick={() => handleRemoveTodo(todo.todoId)}
+                    className="bg-green-500 rounded-lg px-2 p-1 hover:bg-green-800 hover:text-white"
+                  >
                     Yes
                   </button>
-                  <button onClick={() => setRemovalId("")}>No</button>
-                </>
+                  <button
+                    onClick={() => setRemovalId("")}
+                    className="bg-red-400 rounded-lg px-2 p-1 hover:bg-red-800"
+                  >
+                    No
+                  </button>
+                </div>
               )}
-            </>
+            </div>
           )
         )}
       </div>
