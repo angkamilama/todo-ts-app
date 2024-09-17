3e73726dec29f5f26e67a22a26d9b7b54dc58600
angkamilama <angkami46@gmail.com>
2024-09-18 00:09:36 +0200

    add remove btn and sort()

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState, useReducer } from "react";
 import { Input } from "./components/ui/input";
 import { v4 as uuidv4 } from "uuid";
 
-const ADD_TODO = "ADD_TODO";
-const REMOVE_TODO = "REMOVE_TODO";
-const EDIT_TODO = "EDIT_TODO";
+enum ActionTypes {
+  ADD_TODO = "ADD_TODO",
+  REMOVE_TODO = "REMOVE_TODO",
+  EDIT_TODO = "EDIT_TODO",
+}
 
 interface Todo {
   todoItem: string;
@@ -16,13 +18,16 @@ const initialState: Todo[] = [];
 
 const reducer = (state: Todo[], action: Todo) => {
   switch (action.type) {
-    case ADD_TODO: {
+    case ActionTypes.ADD_TODO: {
       return [...state, { todoItem: action.todoItem, id: action.id }];
     }
-    case EDIT_TODO: {
+    case ActionTypes.EDIT_TODO: {
       return state.map((item) =>
         item.id === action.id ? { ...item, todoItem: action.todoItem } : item
       );
+    }
+    case ActionTypes.REMOVE_TODO: {
+      return state.filter((item) => item.id !== action.id);
     }
     default:
       return state;
@@ -31,68 +36,94 @@ const reducer = (state: Todo[], action: Todo) => {
 
 function App() {
   const [todoItem, setTodoItem] = useState("");
-  const [todo, dispatch] = useReducer(reducer, initialState);
   const [editedTodoItem, setEditedTodoItem] = useState("");
   const [editedId, setEditedId] = useState("");
+  const [todo, dispatch] = useReducer(reducer, initialState);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!todoItem) return;
     let todoId: string = uuidv4();
-    dispatch({ type: ADD_TODO, todoItem: todoItem, id: todoId });
+    dispatch({ type: ActionTypes.ADD_TODO, todoItem: todoItem, id: todoId });
     setTodoItem("");
   };
 
-  const handleEditedInput = (id: string) => {
-    setEditedId(id);
-  };
+  const sortedTodo = [...todo].sort((a, b) => {
+    const nameA = a.todoItem.toLowerCase();
+    const nameB = b.todoItem.toLowerCase();
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
+  });
+
   return (
     <>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Todo:
+      <div className="w-full h-screen flex flex-col justify-center items-center box-border">
+        <form
+          onSubmit={handleSubmit}
+          className=" p-3 w-1/3 flex justify-around items-center mb-5"
+        >
+          <label className="flex justify-between items-center w-2/3">
+            TODO
             <Input
               type="text"
               value={todoItem}
               onChange={(e) => setTodoItem(e.target.value)}
+              className="ml-5 "
             />
           </label>
-          <button className="border border-slate-700 bg-slate-300 p-1 rounded-lg">
+          <button className="border border-slate-700 bg-slate-300 px-3 py-1 rounded-lg">
             Add
           </button>
         </form>
-        <ul>
-          {todo.map((item) => (
-            <li key={item.id}>
+        <ul className=" w-5/12 h-40">
+          {sortedTodo.map((item) => (
+            <li key={item.id} className="h-12 flex items-center">
               {editedId === item.id ? (
-                <div>
+                <div className="flex justify-evenly items-center w-10/12 mx-auto mb-2">
                   <Input
                     type="text"
                     value={editedTodoItem}
                     onChange={(e) => setEditedTodoItem(e.target.value)}
+                    className="w-9/12"
                   />
                   <button
-                    className="border border-slate-700 bg-slate-300 p-1 rounded-lg"
+                    className="border border-slate-700 bg-slate-300 px-1 rounded-lg"
                     onClick={() => {
                       dispatch({
-                        type: EDIT_TODO,
+                        type: ActionTypes.EDIT_TODO,
                         id: item.id,
                         todoItem: editedTodoItem,
                       });
-                      setEditedId(" "); // Reset the editedId after changing the todo
+                      setEditedId(" ");
                     }}
                   >
-                    Change
+                    Update
                   </button>
                 </div>
               ) : (
-                <div>
-                  {item.todoItem}
-                  <button onClick={() => handleEditedInput(item.id)}>
-                    Edit
-                  </button>
-                  <button>Remove</button>
+                <div className=" w-10/12 p-1 flex justify-evenly items-center mx-auto">
+                  <p className="w-6/12 p-1 text-center"> {item.todoItem}</p>
+                  <div className="flex justify-evenly items-center w-4/12 ">
+                    <button
+                      onClick={() => setEditedId(item.id)}
+                      className="border border-slate-700 bg-slate-300 px-3  rounded-lg"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() =>
+                        dispatch({
+                          type: ActionTypes.REMOVE_TODO,
+                          id: item.id,
+                          todoItem: item.todoItem,
+                        })
+                      }
+                      className="border border-slate-700 bg-slate-300 px-1 rounded-lg ml-5"
+                    >
+                      Remove
+                    </button>
+                  </div>
                 </div>
               )}
             </li>
